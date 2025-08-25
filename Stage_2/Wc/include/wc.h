fdd6fec2d7ce18c359d989cf5bc76b68d9e00376
Jules Goy <julesgoydev@gmail.com>
2025-08-25 13:38:13 +0200

    Added wc command and remove the executables

--- Stage_2/Wc/include/wc.h
+++ Stage_2/Wc/include/wc.h
@@ -2,5 +2,7 @@
 #define WC_H
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
 
 #endif
--- Stage_2/Wc/src/wc.c
+++ Stage_2/Wc/src/wc.c
@@ -2,5 +2,62 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc <= 1)
+    {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+
+    char *filename = argv[1];
+
+    // Open file
+    FILE *file = fopen(filename, "r");
+    if (file == NULL)
+    {
+        perror(filename);
+        return 1;
+    }
+
+    int newline_counter = 0;
+    int word_counter = 0;
+    int byte_counter = 0;
+
+    // Get file size
+    struct stat st;
+    if (stat(filename, &st) == -1)
+    {
+        perror("stat");
+        fclose(file);
+        return 1;
+    }
+    byte_counter = st.st_size;
+
+    // Count words & lines
+    int c;
+    int in_word = 0;
+    while ((c = fgetc(file)) != EOF)
+    {
+        // Count lines
+        if (c == '\n')
+        {
+            newline_counter++;
+        }
+
+        // Check delimiters manually (space, tab, newline)
+        if (c == ' ' || c == '\t' || c == '\n')
+        {
+            in_word = 0; // we left a word
+        }
+        else if (!in_word)
+        {
+            in_word = 1; // we entered a new word
+            word_counter++;
+        }
+    }
+
+    fclose(file);
+
+    printf("%d %d %d %s\n", newline_counter, word_counter, byte_counter, filename);
+
     return 0;
 }
